4003a3602fed9fec5bb80cdcf4cbf11f390aff69
Damian Sobieralski <dsobiera@iu.edu>
2022-08-22 18:19:39 -0400

    removed unneeded import

--- src/test/java/edu/iu/uits/lms/hierarchyresourcemanager/services/TestConfig.java
+++ src/test/java/edu/iu/uits/lms/hierarchyresourcemanager/services/TestConfig.java
@@ -1,6 +1,5 @@
 package edu.iu.uits.lms.hierarchyresourcemanager.services;
 
-import edu.iu.uits.lms.email.service.EmailService;
 import edu.iu.uits.lms.hierarchyresourcemanager.repository.UserRepository;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;

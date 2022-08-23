d0cdd6def7f37268b0c9df3986f531828a63d144
Damian Sobieralski <dsobiera@iu.edu>
2022-08-23 11:09:38 -0400

    change to use static import

--- src/test/java/edu/iu/uits/lms/hierarchyresourcemanager/services/swagger/SwaggerTestUtil.java
+++ src/test/java/edu/iu/uits/lms/hierarchyresourcemanager/services/swagger/SwaggerTestUtil.java
@@ -1,17 +1,16 @@
 package edu.iu.uits.lms.hierarchyresourcemanager.services.swagger;
 
-import edu.iu.uits.lms.email.EmailConstants;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import static edu.iu.uits.lms.email.EmailConstants.EMAIL_GROUP_CODE_PATH;
 import static edu.iu.uits.lms.iuonly.IuCustomConstants.IUCUSTOM_GROUP_CODE_PATH;
 
 public class SwaggerTestUtil {
    protected static  List<String> getEmbeddedSwaggerToolPaths(List<String> baseList) {
       List<String> expandedList = new ArrayList<>(baseList);
 
-      expandedList.add(EmailConstants.EMAIL_GROUP_CODE_PATH);
+      expandedList.add(EMAIL_GROUP_CODE_PATH);
       expandedList.add(IUCUSTOM_GROUP_CODE_PATH);
 
       return expandedList;

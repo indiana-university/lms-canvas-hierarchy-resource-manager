5509cdf41123079cf5c7d620c30c12c47cc6aa3a
chmaurer <chmaurer@iu.edu>
2022-08-26 15:26:30 -0400

    LMSA-8538 - Moved some stuff out of the rest endpoint here, over to iu-custom

--- src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/rest/HierarchyResourceManagerRestController.java
+++ src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/rest/HierarchyResourceManagerRestController.java
@@ -1,6 +1,5 @@
 package edu.iu.uits.lms.hierarchyresourcemanager.rest;
 
-import edu.iu.uits.lms.canvas.services.CourseService;
 import edu.iu.uits.lms.hierarchyresourcemanager.amqp.CourseTemplateMessageSender;
 import edu.iu.uits.lms.hierarchyresourcemanager.services.HierarchyResourceException;
 import edu.iu.uits.lms.hierarchyresourcemanager.services.NodeManagerService;
@@ -22,11 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping({"/rest/hrm"})
-//@LmsSwaggerDocumentation
 public class HierarchyResourceManagerRestController {
 
     @Autowired
@@ -39,26 +36,7 @@ public class HierarchyResourceManagerRestController {
     private NodeManagerService hierarchyResourceService;
 
     @Autowired
-    private CourseService courseService;
-
-    @Autowired
     private CourseTemplateMessageSender courseTemplateMessageSender;
-
-    @RequestMapping(value = "/all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<HierarchyResource> getAllNodes() {
-        List<HierarchyResource> hierarchyResources = (List<HierarchyResource>) hierarchyResourceRepository.findAll();
-        return hierarchyResources;
-    }
-
-    @RequestMapping(value = "/resourceId/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public HierarchyResource getNodeFromId(@PathVariable Long id) {
-        return hierarchyResourceRepository.findById(id).orElse(null);
-    }
-
-    @RequestMapping(value = "/{nodeName}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<HierarchyResource> getNodeFromNodeName(@PathVariable String nodeName) {
-        return hierarchyResourceRepository.findByNode(nodeName);
-    }
 
     @RequestMapping(value = "/iuSiteId/{iuSiteId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity getNodeFromIuSiteId(@PathVariable String iuSiteId) {

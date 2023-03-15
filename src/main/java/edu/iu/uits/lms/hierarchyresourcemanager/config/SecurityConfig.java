8b7738ec2ef2696aaba30539410873ff3515ed0f
Damian Sobieralski <dsobiera@iu.edu>
2023-03-15 18:33:08 -0400

    LMSA-8809 - it12-ified

--- src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/config/SecurityConfig.java
@@ -33,6 +33,7 @@ package edu.iu.uits.lms.hierarchyresourcemanager.config;
  * #L%
  */
 
+import edu.iu.uits.lms.common.it12logging.RestSecurityLoggingConfig;
 import edu.iu.uits.lms.common.oauth.CustomJwtAuthenticationConverter;
 import edu.iu.uits.lms.hierarchyresourcemanager.repository.UserRepository;
 import edu.iu.uits.lms.lti.repository.DefaultInstructorRoleRepository;
@@ -70,6 +71,8 @@ public class SecurityConfig {
                     .and()
                     .oauth2ResourceServer()
                     .jwt().jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+
+            http.apply(new RestSecurityLoggingConfig());
         }
     }
 

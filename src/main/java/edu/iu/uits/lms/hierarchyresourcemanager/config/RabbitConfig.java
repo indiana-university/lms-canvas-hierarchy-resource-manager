de130f217a5e49170d0a29a6fe1a53a61bc61584
Wagner <wagnermr@iu.edu>
2024-01-05 09:31:27 -0700

    LMSA-7749 upgrade spring-cloud and add rabbit message converter

--- src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/config/RabbitConfig.java
+++ src/main/java/edu/iu/uits/lms/hierarchyresourcemanager/config/RabbitConfig.java
@@ -33,7 +33,9 @@ package edu.iu.uits.lms.hierarchyresourcemanager.config;
  * #L%
  */
 
+import edu.iu.uits.lms.common.coursetemplates.CourseTemplateMessage;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -47,5 +49,12 @@ public class RabbitConfig {
    @Bean(name = "courseTemplateQueue")
    Queue courseTemplateQueue() {
       return new Queue(toolConfig.getCourseTemplateQueueName());
+   }
+
+   @Bean
+   public SimpleMessageConverter converter() {
+      SimpleMessageConverter converter = new SimpleMessageConverter();
+      converter.addAllowedListPatterns(CourseTemplateMessage.class.getName());
+      return converter;
    }
 }

ca0beb29a461077a5a37fb9f498d5d13674d8943
Wagner <wagnermr@iu.edu>
2024-04-03 10:03:13 -0700

    LMSA-9201 revert back to old focus strategy

--- src/main/js/components/coursetemplating/ApplyTemplate.js
+++ src/main/js/components/coursetemplating/ApplyTemplate.js
@@ -88,8 +88,9 @@ class ApplyTemplate extends React.Component {
         const nodeName = event.target.getAttribute("data-node-name");
         const triggerId = "apply-" + templateId;
 
-        // move focus to the first paragraph
-        var dialogHeading = $("#dialog-focus").focus();
+        // move focus to the dialog heading
+        var dialogHeading = $("#apply-template-dialog").find("h1.rvt-dialog__title").first();
+        dialogHeading.focus();
 
         // When the rivet dialog closes, it always is the last event.  So when we try to manually move
         // the focus on cancel, it gets overridden when the modal closes (the first instance of data-rvt-dialog-trigger always gets focus).
@@ -158,7 +159,7 @@ class ApplyTemplate extends React.Component {
                         <React.Fragment>
                             <div id="templateWarning">
                                 <span className="rvt-text-bold">{this.state.modalData.nodeName} - {this.state.modalData.templateName}</span>
-                                <p id="dialog-focus" tabindex="-1">
+                                <p>
                                     Please note that this action may make changes to your course navigation (including removing the IU
                                     eTexts tool from the navigation of courses that use eTexts), course settings, and/or course content.
                                     If you are uncertain about the impact this template will have on your course, you may want to consider

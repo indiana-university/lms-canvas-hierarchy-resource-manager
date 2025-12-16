0943225cc00c9d14667026faef8be941fa92c2fe
Wagner <wagnermr@iu.edu>
2025-12-16 09:36:06 -0800

    LMSA-9585 handle alert dismissal with react instead of rivet

--- src/main/js/components/SuccessAlert.js
+++ src/main/js/components/SuccessAlert.js
@@ -33,22 +33,9 @@
 import React from 'react'
 
 class SuccessAlert extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidUpdate() {
-        const thisAlert = document.querySelector('[data-rvt-alert="success"]');
-        if (thisAlert && this.props.onDismiss) {
-            thisAlert.addEventListener('rvtAlertDismissed', this.props.onDismiss);
-        }
-    }
-
-    componentDidUnmount() {
-        const thisAlert = document.querySelector('[data-rvt-alert="success"]');
-        if (thisAlert && this.props.onDismiss) {
-            thisAlert.removeEventListener('rvtAlertDismissed', this.props.onDismiss);
+    handleDismiss = (event) => {
+        if (this.props.onDismiss) {
+            this.props.onDismiss(event);
         }
     }
 
@@ -59,7 +46,7 @@ class SuccessAlert extends React.Component {
                 <div className="rvt-alert rvt-alert--success [ rvt-m-top-md ]" aria-labelledby="success-alert-title" data-rvt-alert="success" tabIndex="-1">
                   <div className="rvt-alert__title" id="success-alert-title">{this.props.alertTitle}</div>
                   <p className="rvt-alert__message">{this.props.alertMessage}</p>
-                  <button className="rvt-alert__dismiss" data-rvt-alert-close >
+                  <button className="rvt-alert__dismiss" onClick={this.handleDismiss}>
                     <span className="rvt-sr-only">Close</span>
                     <svg fill="currentColor" width="16" height="16" viewBox="0 0 16 16"><path d="m3.5 2.086 4.5 4.5 4.5-4.5L13.914 3.5 9.414 8l4.5 4.5-1.414 1.414-4.5-4.5-4.5 4.5L2.086 12.5l4.5-4.5-4.5-4.5L3.5 2.086Z"></path></svg>
                   </button>
